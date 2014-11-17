a6b4d0602355d211380a73ad03acc43a43f8ead6
Albert Decatur <adecatur@aiddata.org>
2014-11-17 17:36:28 -0500

    script name

--- scratch/aid/aid2geom.sh
+++ scratch/aid/aid2geom.sh
@@ -5,6 +5,7 @@
 # assign financials based on input field of users choice - assumed to be at project level, distributed evenly across project locations
 # inputs: postgres table with { projectid, financials column, precision code, lat, lng }, **and** allgeom table in postgis from GAUL boundaries with adm_level field (0|1|2) and geom field
 # output: raster with user's choice of x/y res with financials by pixel
+# NB: input aid table must have valid lng,lat, be wgs84, prec code must be {1,2,3,4,5,6,8}, financials must refer to project level, aid must fall within an allgeom to be considered when prec code is {3,4}
 # example use: $0
 
 inaid=m4r
