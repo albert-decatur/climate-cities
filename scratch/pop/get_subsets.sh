1c8da497f12f29dc0b8717819da7b077f2787144
Albert Decatur <adecatur@aiddata.org>
2015-01-29 15:07:05 -0500

    script to get, for each shp city poly, the corresponding raster for ppt prctdiff1 and prctdiff2, ts, and aid

---
+++ scratch/pop/get_subsets.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+a="'";outdir=cities; inshp=geonames_PPLge10k_by_GRUMPpoly.shp; ogrinfo -geom=no -ro -al -sql "select GID_GRUMP from $(basename $inshp .shp)" $inshp | grep GID_GRUMP| sed '1d' | grep -oE "[0-9]+"|parallel 'ogr2ogr -sql "select * from $(basename '$inshp' .shp) where GID_GRUMP = '$a'{}'$a'" '$outdir'/{}.shp '$inshp''
