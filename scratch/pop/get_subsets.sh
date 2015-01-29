5e2a3225dddd8e67da2562c4fa203b15d8fb3b97
Albert Decatur <adecatur@aiddata.org>
2015-01-29 15:09:19 -0500

    raise gnu flag for parallel

--- scratch/pop/get_subsets.sh
+++ scratch/pop/get_subsets.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-a="'";outdir=cities; inshp=geonames_PPLge10k_by_GRUMPpoly.shp; ogrinfo -geom=no -ro -al -sql "select GID_GRUMP from $(basename $inshp .shp)" $inshp | grep GID_GRUMP| sed '1d' | grep -oE "[0-9]+"|parallel 'ogr2ogr -sql "select * from $(basename '$inshp' .shp) where GID_GRUMP = '$a'{}'$a'" '$outdir'/{}.shp '$inshp''
+a="'"
+outdir=cities
+inshp=geonames_PPLge10k_by_GRUMPpoly.shp
+ogrinfo -geom=no -ro -al -sql "select GID_GRUMP from $(basename $inshp .shp)" $inshp |\
+grep GID_GRUMP|\
+sed '1d' |\
+grep -oE "[0-9]+"|\
+parallel --gnu 'ogr2ogr -sql "select * from $(basename '$inshp' .shp) where GID_GRUMP = '$a'{}'$a'" '$outdir'/{}.shp '$inshp''
